c49d1bece7997280bfa223a3e97d4e543134cf57
devProjetsAlex <alexandre.cote.5@gmail.com>
2020-05-07 14:58:27 -0400

    administration et client

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,4 +1,12 @@
 class ClientController < ApplicationController
   def reservation
   end
+
+  def new_reservation
+  end
+
+  def client_reservation
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'administration/action'
   root 'client#reservation'
+  get 'administration/action'
+  get 'new_reservation', to: 'client#reservation'
+  get 'my_reservation', to: 'client#new_reservation'
+  get 'client_reservation', to: 'client#client_reservation'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
